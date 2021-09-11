7da85f714836569c30e10d5701ce2f9e9f31b5e4
Thalyson <=>
2021-09-11 11:12:41 -0300

    Subindo projeto.

---
+++ README.md
@@ -0,0 +1,1 @@
+#API REST com PHP e MySQL
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+
+    const DBDRIVE = 'mysql';
+    const DBHOST = 'localhost';
+    const DBNAME = 'db_api_rest_one';
+    const DBUSER = '';
+    const DBPASS = '';
